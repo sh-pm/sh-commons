6d9ac8c2c9266f1450ad94ea7ad418ca83d84334
Arildo <arildo.gueno@gmail.com>
2020-11-09 20:26:17 -0200

    v2.0.0
    Nem version not compatible with old versions: refactoring in function
    names and file names

--- src/test/sh/log_util_test.sh
+++ src/test/sh/log_util_test.sh
@@ -6,8 +6,8 @@ include_lib sh-logger
 include_lib sh-unit
 
 # SUT's
-include_file $SRC_DIR_PATH/log_utils.sh
-include_file $SRC_DIR_PATH/string_utils.sh
+include_file $SRC_DIR_PATH/log_util.sh
+include_file $SRC_DIR_PATH/string_util.sh
 
 test_get_script_caller_name() {
 	local RETURN=$( get_script_caller_name /tmp )
