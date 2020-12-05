a0073d5701b40f73e60804a15f98876e21395722
sh-pm <shpmmail@gmail.com>
2020-12-05 10:29:22 -0300

    v2.2.2 - Fixes
    - param_util.sh - ensure_number_params_correct: handle "multi-word"
    params,
    because a only 1 param with 2 or+ words passed a function, inside
    function in ?@ turns into 2 or + params instead only 1!
    
    - fix unit tests breaked after function name refactoring's.

--- src/main/sh/param_util.sh
+++ src/main/sh/param_util.sh
@@ -57,14 +57,16 @@ is_number_params_correct() {
  ##
 ensure_number_params_correct(){
 	
-	if [ $# -eq 0 ]; then
+	local ARGS=("$@")
+	
+	if [ ${#ARGS[@]} -eq 0 ]; then
 		local NUM_PARAMS_EXPECTED=0
 	else 
 		local NUM_PARAMS_EXPECTED=$1
 	fi
 	
     local FUNCTION_NAME=${FUNCNAME[1]}
-    local NUM_PARAMS_RECEIVED=$(( $# - 1 )) # the first param is not a param of function under avaliation, is the number of param expected
+    local NUM_PARAMS_RECEIVED=$(( ${#ARGS[@]} - 1 )) # the first param is not a param of function under avaliation, is the number of param expected
 
 	if [ $NUM_PARAMS_RECEIVED -ne $NUM_PARAMS_EXPECTED ]; then	    
 		log_error "Illegal number of parameters. Function '$FUNCTION_NAME' expect $NUM_PARAMS_EXPECTED params but receive $NUM_PARAMS_RECEIVED"	    
--- src/test/sh/file_util_test.sh
+++ src/test/sh/file_util_test.sh
@@ -8,29 +8,29 @@ include_lib sh-unit
 # SUT's
 include_file $SRC_DIR_PATH/file_util.sh
 
-test_is_directory() {
-	is_directory /tmp
+test_is_existing_folder() {
+	is_existing_folder /tmp
 	assert_true $?
 	
-	is_directory /tmpx
+	is_existing_folder /tmpx
 	assert_false $?
 }
 
-test_directory_is_empty() {
+test_is_empty_folder() {
 	
 	FOLDER4TEST=/tmp/folder4test
 
-	if $( directory_exists $FOLDER4TEST ); then
+	if $( is_existing_folder $FOLDER4TEST ); then
 		rm -rf $FOLDER4TEST
 	fi
 	
 	mkdir $FOLDER4TEST
 	
-	directory_is_empty $FOLDER4TEST	
+	is_empty_folder $FOLDER4TEST	
 	assert_true $?
 	
 	touch $FOLDER4TEST/lixotmp.txt
-	directory_is_empty $FOLDER4TEST
+	is_empty_folder $FOLDER4TEST
 	assert_false $?
 }
 
